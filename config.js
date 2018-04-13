ace7bb6da03db4be51adfdcd1d9a978032ae1605
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-13 22:47:08 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_id': '233918217420-imrdt9jhdeqnnir899aio3ctfa969iea.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'XcfB8oeSjqg3ywlDVH1t-g4K ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback' 
+};
